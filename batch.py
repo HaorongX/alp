9442c84a1d08585dfec1df6c2a7c30d09cb2ca8e
Haorong Xu <db_haorong@outlook.com>
2025-04-20 12:45:13 +0800

    Stop tracking batch.py

--- batch.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for i in ['w']:
-    for j in ['24']:
-        for k in ['11', '12', '13', '21', '22', '23', '31', '32', '33']:
-            name = f"./9618_{i}{j}_qp_{k}.pdf"
-            os.system(f"python3 preprocessing.py {name}")
-            os.system(f"python3 extractqp.py {name.replace('.pdf', '_processed.pdf')}")
