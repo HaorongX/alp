7a48079843038cc33951c0c1ae1d9cb1926c54c9
Haorong Xu <db_haorong@outlook.com>
2025-10-25 14:06:18 +0800

    Add word stemming, removing stop words.

--- categorize_pipeline/categorizing.py
+++ categorize_pipeline/categorizing.py
@@ -2,6 +2,9 @@ import requests
 import json
 import re
 import unicodedata
+from nltk.corpus import stopwords
+import nltk
+from sklearn.preprocessing import LabelEncoder
 
 def clean_string(text):
     cleaned_text = ''.join(ch for ch in text if unicodedata.category(ch)[0] != 'C') # Remove control characters
@@ -12,10 +15,23 @@ def clean_string(text):
     cleaned_text = re.sub(r'([a-z])([A-Z])', r'\1 \2', cleaned_text)  # add missing spaces
     cleaned_text = re.sub(r'\[\s*\d+\s*\]', '', cleaned_text) # mark worth
     cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
-    return cleaned_text.lower()
+    cleaned_text = re.sub(r' - ', ' ', cleaned_text) # isolate dash
+    cleaned_text = cleaned_text.lower()
+    try:
+        stop_words = stopwords.words('english')
+        cleaned_text = ' '.join(word for word in cleaned_text.split(' ') if word not in stop_words)
+    except LookupError:
+        nltk.download('stopwords')
+        stop_words = stopwords.words('english')
+        cleaned_text = ' '.join(word for word in cleaned_text.split(' ') if word not in stop_words)
+    return cleaned_text
+
+def stemm_text(text):
+    global stemm_text
+    text = ' '.join(stemmer.stem(word) for word in text.split(' '))
+    return text
 
 def ocr(base64_img):
-    global config
     payload={
         'language': 'eng',
         'isOverlayRequired': 'false',
@@ -31,7 +47,13 @@ def ocr(base64_img):
     response = requests.request("POST", url, headers=headers, data=payload)
     return clean_string(json.loads(response.text, strict = False)["ParsedResults"][0]["ParsedText"])
 
-config = json.load(open("config.json"))
+def __init__():
+    global config
+    global stemmer
+    stemmer = nltk.SnowballStemmer("english")
+    config = json.load(open("config.json"))
+
+__init__()
 with open("./sample_data.txt", "r") as f:
     sample = f.read()
-print(ocr(sample))
+print(stemm_text(ocr(sample)))
