1bd204c2a45e3a348d9c0a1206b78f71c20fa39d
Haorong Xu <db_haorong@outlook.com>
2025-10-25 13:35:42 +0800

    OCR wrapper and denoise (clean data) function

--- categorize_pipeline/categorizing.py
+++ categorize_pipeline/categorizing.py
@@ -1,32 +1,37 @@
-import re
-import cv2
 import requests
 import json
-import sys
-import os
-import sqlite3
+import re
+import unicodedata
 
+def clean_string(text):
+    cleaned_text = ''.join(ch for ch in text if unicodedata.category(ch)[0] != 'C') # Remove control characters
+    cleaned_text = re.sub(r'\s+', ' ', cleaned_text).strip() # Merge multiple spaces and strip leading/trailing whitespace
+    cleaned_text = re.sub(r'\(\s*[a-z]+\s*\)', '', cleaned_text) # Question number
+    cleaned_text = re.sub(r'^\d+', '', cleaned_text) # remove leading question numbers
+    cleaned_text = cleaned_text.replace('.', '') # Remove any dot
+    cleaned_text = re.sub(r'([a-z])([A-Z])', r'\1 \2', cleaned_text)  # add missing spaces
+    cleaned_text = re.sub(r'\[\s*\d+\s*\]', '', cleaned_text) # mark worth
+    cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
+    return cleaned_text.lower()
 
-payload={
-    'language': 'eng',
-    'isOverlayRequired': 'false',
-    'iscreatesearchablepdf': 'false',
-    'issearchablepdfhidetextlayer': 'false',
-    'isTable': 'true',
-    'OCREngine': '2',
-    'base64Image': 'data:image/png;base64,'
-}
+def ocr(base64_img):
+    global config
+    payload={
+        'language': 'eng',
+        'isOverlayRequired': 'false',
+        'iscreatesearchablepdf': 'false',
+        'issearchablepdfhidetextlayer': 'false',
+        'isTable': 'true',
+        'OCREngine': '2',
+        'base64Image': 'data:image/png;base64,'
+    }
+    url = config["OCRendPoint"]
+    headers = { 'apikey': config["OCRkey"] }
+    payload['base64Image'] += base64_img
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return clean_string(json.loads(response.text, strict = False)["ParsedResults"][0]["ParsedText"])
 
 config = json.load(open("config.json"))
-url = config["OCRendPoint"]
-
-headers = {
-  'apikey': config["OCRkey"]
-}
-
 with open("./sample_data.txt", "r") as f:
     sample = f.read()
-    payload['base64Image'] += sample
-
-response = requests.request("POST", url, headers=headers, data=payload)
-print(json.loads(response.text, strict = False)["ParsedResults"][0]["ParsedText"].strip())
+print(ocr(sample))
